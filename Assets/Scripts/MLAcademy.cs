9fdcd98cebe98d5fe2284993f3f90852fa934cb7
Hoooon22 <momo990305@gmail.com>
2021-08-05 13:24:35 +0900

    using issue fix

--- Assets/Scripts/MLAcademy.cs
+++ Assets/Scripts/MLAcademy.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MLAgents; // ML-Agents의 Academy 기능
+using Unity.MLAgents; // ML-Agents의 Academy 기능
 
 public class MLAcademy : MonoBehaviour
 {
